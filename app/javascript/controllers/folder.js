b8a1a383c4d99e03a73f53293c0bcb28930d1cc1
saigo03 <149645624+saigo03@users.noreply.github.com>
2023-12-14 04:34:14 +0000

    フォルダ名popup作成（途中)

---
+++ app/javascript/controllers/folder.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.getElementById("create-folder-btn");
+  const createForm = document.getElementById("create-folder-form");
+  const overlay = document.getElementById("overlay");
+
+  if (createButton && createForm && overlay) {
+    createButton.addEventListener("click", () => {
+      createForm.style.display = "block";
+      overlay.style.display = "block";
+    });
+
+    overlay.addEventListener("click", () => {
+      createForm.style.display = "none";
+      overlay.style.display = "none";
+    });
+  }
+});
