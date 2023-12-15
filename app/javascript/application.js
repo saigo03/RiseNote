1e5cbe44a2c46468a38e13ea754f3a97cdecb6a1
saigo03 <149645624+saigo03@users.noreply.github.com>
2023-12-15 03:51:16 +0000

    フォルダ名ポップアップ実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// 下記を有効にすると削除機能に問題が発生する
 import "@hotwired/turbo-rails"
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
 import "controllers/folder"
 
