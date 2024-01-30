1101893e720c5b8ec48642649f65a518c649dd19
keitaro <147474084+Nakaharakeitaro@users.noreply.github.com>
2024-01-30 05:03:05 +0000

    ログインページ目隠し追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin_all_from 'app/javascript/memo', under: 'memo'
+pin_all_from 'app/javascript/login_form', under: 'login_form'
